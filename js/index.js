e04a4d8b0d6e8f15db6fd44f0dc5e784f0be7567
Matheus Dumont <95390248+Dumont19@users.noreply.github.com>
2023-06-04 01:21:39 -0300

    First version

---
+++ js/index.js
@@ -0,0 +1,135 @@
+const calculator = () => {
+  const body = document.querySelector('body')
+  const title = document.querySelector('h1')
+  const themeParag = document.querySelector('p')
+  const numbers = Array.from(document.querySelectorAll('small'))
+  const selectorBg = document.querySelector('.selector-bg')
+  const selectors = Array.from(document.querySelectorAll('.selector-circle'))
+  const screen = document.querySelector('input[type="text"]')
+  const keysBg = document.querySelector('.keys')
+  const keys = Array.from(document.querySelectorAll('.key'))
+  const del = document.querySelector('#delete')
+  const reset = document.querySelector('#reset')
+  const equal = document.querySelector('#equal')
+
+  const selectTheme = () => {
+    selectors.forEach((selector) => {
+      selector.addEventListener('click', (e) => {
+        if (e.target === selectors[0]) {
+          selectors[1].style.opacity = '0'
+          selectors[2].style.opacity = '0'
+          selectors[0].style.opacity = '1'
+          body.style.background = 'var(--very-dark-desaturated-blue-body)'  
+          title.classList.remove('theme-two')
+          themeParag.classList.remove('theme-two')
+          selectors.forEach((selector) =>
+            selector.classList.remove('theme-two')
+          )
+          numbers.forEach((number) => number.classList.remove('theme-two'))
+          selectorBg.classList.remove('theme-two')
+          screen.classList.remove('theme-two')
+          keysBg.classList.remove('theme-two')
+          keys.forEach((key) => key.classList.remove('theme-two'))
+
+          title.classList.remove('theme-three')
+          themeParag.classList.remove('theme-three')
+          selectors.forEach((selector) =>
+            selector.classList.remove('theme-three')
+          )
+          numbers.forEach((number) => number.classList.remove('theme-three'))
+          selectorBg.classList.remove('theme-three')
+          screen.classList.remove('theme-three')
+          keysBg.classList.remove('theme-three')
+          keys.forEach((key) => key.classList.remove('theme-three'))
+        } else if (e.target === selectors[1]) {
+          selectors[0].style.opacity = '0'
+          selectors[2].style.opacity = '0'
+          selectors[1].style.opacity = '1'
+
+          title.classList.remove('theme-three')
+          themeParag.classList.remove('theme-three')
+          selectors.forEach((selector) =>
+            selector.classList.remove('theme-three')
+          )
+          numbers.forEach((number) => number.classList.remove('theme-three'))
+          selectorBg.classList.remove('theme-three')
+          screen.classList.remove('theme-three')
+          keysBg.classList.remove('theme-three')
+          keys.forEach((key) => key.classList.remove('theme-three'))
+
+          body.style.background = 'var(--light-gray)'
+          title.classList.add('theme-two')
+          themeParag.classList.add('theme-two')
+          selectors.forEach((selector) => selector.classList.add('theme-two'))
+          numbers.forEach((number) => number.classList.add('theme-two'))
+          selectorBg.classList.add('theme-two')
+          screen.classList.add('theme-two')
+          keysBg.classList.add('theme-two')
+          keys.forEach((key) => key.classList.add('theme-two'))
+        } else {
+          selectors[0].style.opacity = '0'
+          selectors[1].style.opacity = '0'
+          selectors[2].style.opacity = '1'
+
+          title.classList.remove('theme-two')
+          themeParag.classList.remove('theme-two')
+          selectors.forEach((selector) =>
+            selector.classList.remove('theme-two')
+          )
+          numbers.forEach((number) => number.classList.remove('theme-two'))
+          selectorBg.classList.remove('theme-two')
+          screen.classList.remove('theme-two')
+          keysBg.classList.remove('theme-two')
+          keys.forEach((key) => key.classList.remove('theme-two'))
+
+          body.style.background = 'var(--very-dark-violet)'
+          title.classList.add('theme-three')
+          themeParag.classList.add('theme-three')
+          selectors.forEach((selector) => selector.classList.add('theme-three'))
+          numbers.forEach((number) => number.classList.add('theme-three'))
+          selectorBg.classList.add('theme-three')
+          screen.classList.add('theme-three')
+          keysBg.classList.add('theme-three')
+          keys.forEach((key) => key.classList.add('theme-three'))
+        }
+      })
+    })
+  }
+  selectTheme()
+
+  const calculate = () => {
+    keys.forEach(key => {
+        key.addEventListener('click', (e) => {
+
+            if (screen.value === '') {
+                keys[7].value = ''
+                keys[11].value = ''
+                keys[12].value = ''
+                keys[13].value = ''
+                keys[14].value = ''
+                keys[15].value = '' 
+            } else {
+                keys[7].value = '+'
+                keys[11].value = '-'
+                keys[12].value = '.'
+                keys[13].value = '0'
+                keys[14].value = '/'
+                keys[15].value = '*' 
+            }
+    
+            if (e.target.value === 'delete') {
+                screen.value = screen.value.slice(0, -1)
+            } else if (e.target.value === 'reset') {
+                screen.value = ''
+            } else if (e.target.value === 'equal') {
+                const result = eval(screen.value)
+                screen.value = result
+            } else {
+                screen.value += e.target.value
+            }
+        })
+    })
+  }
+  calculate()
+}
+calculator()
